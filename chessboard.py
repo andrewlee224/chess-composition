dd25c489f6f119bbff22ce9adca7f44d191a0525
Andrzej Lichaczewski <andrewlee224@gmail.com>
2015-09-25 22:18:33 +0200

    Keeping track of fields threatened multiple times, remove piece method, fixed piece addition logic

--- chessboard.py
+++ chessboard.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Chessboard(object):
     """Represents chessboard
 
@@ -22,15 +25,19 @@ class Chessboard(object):
         self.pieces = []
         self.occupied_positions = set()
         self.threatened_positions = set()
+        # a position may be threatened multiple times, the dict is for keeping
+        # track of this number
+        self.threatened_dict = defaultdict(lambda: 0)
         self._blocked_positions = set()
         self._rows = rows
         self._cols = cols
+        self._num_fields = rows*cols
         self.add_threatened = add_threatened
 
     @property
     def blocked_positions(self):
         if not self.add_threatened:
-            return self.occupied_positions.update(self.threatened_positions)
+            return self.occupied_positions.union(self.threatened_positions)
 
         return self.occupied_positions
 
@@ -45,14 +52,37 @@ class Chessboard(object):
         piece.bound_chessboard = self
         piece.position = pos
 
-        new_threatened_positions = {
-            thr_pos for thr_pos in piece.possible_moves
-            if thr_pos not in self.occupied_positions
-        }
+        # check if new possible moves don't threaten existing pieces
+        if piece.possible_moves.intersection(self.occupied_positions):
+            piece.reset()
+            return False
+
+        new_threatened_positions = piece.possible_moves
 
         self.threatened_positions.update(new_threatened_positions)
+
+        # update the threatened positions counts
+        for position in new_threatened_positions:
+            self.threatened_dict[position] += 1
+
         self.occupied_positions.add(pos)
 
         self.pieces.append(piece)
 
         return True
+
+    def remove(self, piece):
+        if piece not in self.pieces:
+            return False
+
+        self.occupied_positions.remove(piece.position)
+
+        for position in piece.possible_moves:
+            self.threatened_dict[position] -= 1
+            if self.threatened_dict[position] == 0:
+                self.threatened_positions.remove(position)
+
+        piece.reset()
+
+        return True
+
